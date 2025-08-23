2a98e780c2c34e0c6be9884ab56d7dd494d36d9c
alant9966 <alant9966@gmail.com>
2025-08-23 00:51:42 -0700

    Create CarModel, CarMake models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     
     # Path for logout
     path(route='logout', view=views.logout_request, name='logout'),
+    
+    # Path for car information view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # Path for dealer reviews view
 
