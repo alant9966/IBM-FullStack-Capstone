27c0b81bb57d070af382f00bf301a6886eb3dfc5
alant9966 <alant9966@gmail.com>
2025-08-24 00:36:34 -0700

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const fs = require('fs');;
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -22,10 +22,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
@@ -35,7 +35,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -74,7 +74,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
     const dealers = await Dealerships.find({state: req.params.state});
     res.json(dealers);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching dealerships' })
+    res.status(500).json({ error: 'Error fetching dealerships' });
   }
 });
 
@@ -84,21 +84,21 @@ app.get('/fetchDealer/:id', async (req, res) => {
     const dealer = await Dealerships.find({id: req.params.id});
     res.json(dealer);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching dealership' })
+    res.status(500).json({ error: 'Error fetching dealership' });
   }
 });
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0]['id']+1;
 
   const review = new Reviews({
 		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
+		"name": data.name,
+		"dealership": data.dealership,
+		"review": data.review,
 		"purchase": data['purchase'],
 		"purchase_date": data['purchase_date'],
 		"car_make": data['car_make'],
